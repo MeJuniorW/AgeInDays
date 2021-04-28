ec2adfd50879adedde7be90a9bc7d8405bcc9030
Junior Muriithi <murithiwagura@gmail.com>
2021-04-28 20:17:36 +0300

    Get user's birthday and today's date

--- SimplePythonProject/SimplePythonProject.py
+++ SimplePythonProject/SimplePythonProject.py
@@ -1,5 +1,12 @@
+from datetime import datetime
 
-def SayHello(name):
-    print(f"Hello {name}")
-
-SayHello("Junior")
+print("Hello. What is your name?")
+name = input("Name: ")
+print(f"Ok {name}. Tell your birthday and i will tell you how long you have been alive.")
+birth_day = int(input("day: "))
+birth_month = int(input("month: "))
+birth_year = int(input("year: "))
+birth_day_full = datetime(birth_year, birth_month, birth_day)
+print(f"{birth_day_full.day} / {birth_day_full.month} / {birth_day_full.year}")
+today = datetime.today()
+print(f"{today.day} / {today.month} / {today.year}")
