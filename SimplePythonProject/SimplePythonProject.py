944cbc278c7881eecc1dac378f44c2197c05462a
Junior Muriithi <murithiwagura@gmail.com>
2021-04-28 22:48:04 +0300

    function to find the days upto a certain date in a year

--- SimplePythonProject/SimplePythonProject.py
+++ SimplePythonProject/SimplePythonProject.py
@@ -31,4 +31,20 @@ def days_apart_years(starting_year, final_year):
         year += 1
     return days
 
+def days_upto_a_date(day, month, year):
+    days_upto = 0
+    days_with_31_days = {1, 3, 5, 7, 8, 10, 12}
+    for i in range(month+1):
+        if month == 2:
+            if ( ( (year % 4 == 0) and (year % 100 == 0) ) or (year % 400 == 0) ):
+                days_upto = days_upto + 29
+            else:
+                days_upto = days_upto + 28
+        elif month in days_with_30_days:
+            days_upto = days_upto + 30
+        else:
+            days_upto = days_upto + 31
+    days_upto = days_upto - day - 1;
+    return days_upto
+
 Main()
