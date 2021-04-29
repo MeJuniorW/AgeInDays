b89ae9d4ca720a7510f1c181bb6a9c3d07a2a2f2
Junior Muriithi <murithiwagura@gmail.com>
2021-04-29 12:04:15 +0300

    Exception handling done

--- SimplePythonProject/SimplePythonProject.py
+++ SimplePythonProject/SimplePythonProject.py
@@ -5,21 +5,32 @@ def Main():
     print("Hello. What is your name?")
     name = input("Name: ")
     print(f"Ok {name}. Tell me your birthday and i will tell you how many days since your birthday")
-    
-
-    
-    birth_day = int(input("day: "))
-    birth_month = int(input("month: "))
-    birth_year = int(input("year: "))
-    birth = datetime(birth_year, birth_month, birth_day)
     today = datetime.today()
+    while True:
+        try:
+            birth_day = int(input("day: "))
+            birth_month = int(input("month: "))
+            birth_year = int(input("year: "))
+            if birth_month > 12 or birth_month < 0:
+                raise Exception("Not a valid month")
+            if (birth_day > days_in_the_month(birth_month, birth_year)):
+                raise Exception("Not a valid day")
+            if birth_year < 0 or birth_year > today.year:
+                raise Exception("Not a valid year")
+            break
+        except ValueError:
+            print("That could not be converted to an integer")
+        except Exception as instance:
+            x = instance.args
+            print(f"{x}")
 
+    birth = datetime(birth_year, birth_month, birth_day)
+    
     days_between = days_between_years(birth.year, today.year)
     days_upto_today = days_upto_a_date(today.day, today.month, today.year) - 1
     days_upto_birth_day = days_upto_a_date(birth.day, birth.month, birth.year) -1
     days_from_birth_year = days_in_the_year(birth.year) - days_upto_birth_day
     total_days = days_between + days_upto_today + days_from_birth_year
-
     print(f"You have been alive for {total_days} days. Wow.")
 
 def days_in_the_year(year):
@@ -33,7 +44,7 @@ def days_in_the_month(month, year):
     days_with_31_days = {1, 3, 5, 7, 8, 10, 12}
     days_with_30_days = {4, 6, 9, 11}
     if month == 2:
-        if ( ( (year % 4 == 0) and (year % 100 == 0) ) or (year % 400 == 0) ):
+        if ( ( (year % 4 == 0) and (year % 100 != 0) ) or (year % 400 == 0) ):
             days = 29
         else:
             days = 28
